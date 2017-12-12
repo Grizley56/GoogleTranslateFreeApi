749a5cf3bf95c89a68d6859f52947f1bd17780e9
Ilya Meyta <Grizley.medved@gmail.com>
2017-12-12 23:47:57 +0300

    ITranslator interface update

--- src/ITranslator.cs
+++ src/ITranslator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,9 @@ namespace GoogleTranslateFreeApi
 	{
 		Task<TranslationResult> TranslateAsync(ITranslatable item);
 		Task<TranslationResult> TranslateAsync(string text, Language from, Language to);
+		Task<TranslationResult> TranslateLiteAsync(ITranslatable item);
+		Task<TranslationResult> TranslateLiteAsync(string text, Language from, Language to);
+		IWebProxy Proxy { get; }
+		TimeSpan TimeOut { get; }
 	}
 }
